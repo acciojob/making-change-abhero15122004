d4db198f3c2ff7b3f6ed66530464d63c3298b775
Abhinay Reddy <149326091+abhero15122004@users.noreply.github.com>
2026-09-14 18:15:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const makeChange = (c) => {
-  // your name here
-};
+//your JS code here. If required.
 
+function makeChange(amount) {
+    const q = Math.floor(amount / 25);
+    amount = amount % 25;
+
+    const d = Math.floor(amount / 10);
+    amount = amount % 10;
+
+    const n = Math.floor(amount / 5);
+    amount = amount % 5;
+
+    const p = amount;
+
+    return {
+        q: q,
+        d: d,
+        n: n,
+        p: p
+    };
+}
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
